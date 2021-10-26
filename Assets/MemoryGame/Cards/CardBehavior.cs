afc81340ede8550da5885912a0c3eb2d4c12491d
Hardware Tracker <givingtnt@gmail.com>
2021-10-26 15:01:10 -0400

    Final push, end of timer.
    Final game settings, timer UI & card flip behavior.

--- Assets/MemoryGame/Cards/CardBehavior.cs
+++ Assets/MemoryGame/Cards/CardBehavior.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class CardBehavior : MonoBehaviour
 {
-    public bool IsFlipped { get; set; } = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private GameObject gm;
+    
+    private bool isMatched = false;
+    public bool IsMatched {
+        get {
+            return isMatched;
+        }
+        set {
+            isMatched = value;
+            GetComponent<MeshCollider>().enabled = false;
+        }
     }
     
     void OnMouseDown()
     {
+        FlipCard();
+        gm.GetComponent<MemoryGameGM>().CardSelected(gameObject);
+    }
+
+    public void FlipCard()
+    {
+        GetComponent<MeshCollider>().enabled = !GetComponent<MeshCollider>().enabled;
         transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y,transform.localScale.z *-1);
     }
 }
--- Assets/MemoryGame/GameManager/MemoryGameGM.cs
+++ Assets/MemoryGame/GameManager/MemoryGameGM.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class MemoryGameGM : MonoBehaviour
 {
     [SerializeField] private List<CardBehavior> cards = new List<CardBehavior>();
+    [SerializeField] private GameObject endGameOverlay;
+    [SerializeField] private TextMeshProUGUI timer;
+    
+    private GameObject firstCard;
+    private GameObject secondCard;
+    
     private float gameTimer = 0;
+    private int pairsFound = 0;
 
     void Start()
     {
@@ -18,5 +26,55 @@ public class MemoryGameGM : MonoBehaviour
     void Update()
     {
         gameTimer += 1 * Time.deltaTime;
+        timer.text = "temps écoulé: " +  Mathf.Round(gameTimer).ToString();
     }
+
+    public void CardSelected(GameObject mySelectedCard)
+    {
+        if (firstCard == null)
+        {
+            firstCard = mySelectedCard;
+        }
+        else
+        {
+            secondCard = mySelectedCard;
+            VerifyMatch();
+        }
+    }
+
+    private void VerifyMatch()
+    {
+        StartCoroutine("MatchVerifyCoroutine");
+    }
+
+    IEnumerator MatchVerifyCoroutine()
+    {
+        yield return new WaitForSeconds(0.3f);
+        
+        if (firstCard.gameObject.name == secondCard.gameObject.name)
+        {
+            firstCard.GetComponent<CardBehavior>().IsMatched = true;
+            pairsFound++;
+        }
+        else
+        {
+            firstCard.GetComponent<CardBehavior>().FlipCard();
+            secondCard.GetComponent<CardBehavior>().FlipCard();
+        }
+        
+        firstCard = null;
+        secondCard = null;
+
+        if (pairsFound == 8)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        endGameOverlay.SetActive(true);
+    }
+    
+    
 }
